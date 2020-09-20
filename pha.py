3e35cee70ee62765d2e84999bfd3344c2313e018
Claudinei Pereira <contato@claudineipereira.com>
2020-09-19 23:29:27 -0300

    Add while loop

--- pha.py
+++ pha.py
@@ -4,20 +4,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-name = input("Sobrenome, nome: ")
-pha_url = requests.get(f"https://cuttersonline.com/app/generator/?q={name}&ref=pha")
+
+class Bcolors:
+    YELLOW = "\033[93m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
 
 
-def phaResult():
-    soup = BeautifulSoup(pha_url.text, "lxml")
-    pha_number = soup.h4.strong.text
-    print(f"\nCódigo PHA para \"{name}\": {pha_number}")
+while True:
+    try:
 
+        name = input(f"Digite o nome do autor no formato {Bcolors.BOLD}\"Sobrenome, nome\"{Bcolors.ENDC}: ")
 
-def pha():
-    if pha_url.status_code == requests.codes.ok:
-        phaResult()
+        if name == "Q":
+            break
 
+        first = name.split(",")[1].lstrip()
+        last = name.split(",")[0]
+        pha_url = requests.get(f"https://cuttersonline.com/app/generator/?q={name}&ref=pha")
 
-if __name__ == "__main__":
-    pha()
+        if pha_url.status_code == requests.codes.ok:
+
+            soup = BeautifulSoup(pha_url.text, "lxml")
+            pha_number = soup.h4.strong.text
+            print(f"Código PHA para \"{first} {last}\": {Bcolors.YELLOW}{pha_number}{Bcolors.ENDC}\n")
+
+    except IndexError:
+        print(f"Por favor utilize o formato {Bcolors.BOLD}\"Sobrenome, nome\"{Bcolors.ENDC}.")
+
